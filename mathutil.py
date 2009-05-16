1b4d878406157e2419e04a38965159549edf3e8e
Richard Darst <rkd@zgib.net>
2009-05-17 01:19:44 +0300

    add numerically stable averager to mathutil
    darcs-hash:20090516221944-82ea9-2e2c27a7a3b4049b7cc8ffca78471a7640d96b93.gz

--- mathutil.py
+++ mathutil.py
@@ -1,3 +1,4 @@
+# Richard Darst, May 2009
 
 
 def cartesianproduct(*args):
@@ -11,3 +12,50 @@ def cartesianproduct(*args):
         for x in args[0]:
             for xs in cartesian_product_mrgreen2(*args[1:]):
                 yield (x,) + xs
+
+
+class Averager(object):
+    """Numerically Stable Averager
+
+    Calculate averages and standard deviations in a numerically stable way.
+
+    From the 'On-Line Algorithm' from
+    http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+    """
+    def __init__(self):
+        self.n       = 0
+        self._mean    = 0.   # mean
+        self._M2     = 0.   # variance accumulator
+        #self._var_n  = 0.
+        #self._var_n1 = 0.
+    def add(self, value):
+        """Add a new number to the dataset.
+        """
+        n = self.n + 1
+        delta = value - self._mean
+        mean = self._mean + delta/n
+        M2 = self._M2 + delta*(value - mean)
+        self.n = n
+        self._mean = mean
+        self._M2 = M2
+
+    @property
+    def mean(self):
+        """Mean"""
+        return self._mean
+    @property
+    def std(self):
+        """Population Variance"""
+        return math.sqrt(self._M2 / self.n)
+    @property
+    def stdsample(self):
+        """Sample Variance"""
+        return math.sqrt(self._M2 / (self.n-1))
+    @property
+    def var(self):
+        """Population Standard Deviation"""
+        return self._M2 / self.n
+    @property
+    def varsample(self):
+        """Sample Standard Deviation"""
+        return self._M2 / (self.n-1)
