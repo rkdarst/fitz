e7e4208168b6ab7e98b9fab169921e4bc12b4b1f
David M. Creswick <dmc@gyrae.net>
2009-08-25 03:05:41 +0300

    created fitz.misc
    Ignore-this: 838b2a66751730f58e5ce1cd9525a07f
    - assert_directory_exists()
    - findall()
    - Callbacks class
    - cached_property decorator
    - AtomicFileWriter class
    
    darcs-hash:20090825000541-8e86a-cd3b67177ce13937b5a96c1a1f7337085f74e562.gz

---
+++ misc.py
@@ -0,0 +1,172 @@
+'''fitz.misc - various useful (but unrelated) one-off functions and classes
+'''
+import os.path
+
+
+def assert_directory_exists(path):
+    '''create the directory if it does not exist
+
+    designed for *nix, but might be cross-platform. I haven\'t tested.
+    '''
+    if path == '' or path == '/':
+        return
+    if not os.path.exists(path):
+        assert_directory_exists(os.path.dirname(path))
+        os.mkdir(path)
+    elif not os.path.isdir(path):
+        raise AssertionError, '%r is not a directory, but it should be' % path
+
+
+def findall(pattern, data):
+    '''finds all non-overlapping occurances of "pattern" in the string "data"
+
+    return value is a list of re.Match objects
+    '''
+    matches = []
+    pos = 0
+    while True:
+        match = pattern.search(data, pos=pos)
+        if match is None:
+            return matches
+        matches.append(match)
+        pos = match.end(0)
+
+
+class ShortCircuitCallbacks(Exception): pass
+
+class Callbacks:
+    '''Base class for objects that support registering and triggering callbacks
+
+    a bit like the signals on gobjects from glib, but written in python
+    '''
+
+    def __init__(self):
+        self._callbacks = {}
+        self._suppressed_callbacks = []
+
+    def register_callback(self, signal, callback, *args):
+        '''register a function to be called when signal is triggered
+
+        callback should be a callable object. It will be called with
+        the *args given here followed by the *args given by trigger_callback()
+        The **kwargs given to trigger_callback() are also passed.
+
+        The return value is a callable object that may be used to unregister
+        the callback. There is no other way to unregister a callback.
+
+        A callbacks may raise a ShortCircuitCallbacks instance to prevent
+        further callbacks from getting called. The return value of
+        .trigger_callback() will be the first arg passed to
+        ShortCircuitCallbacks.
+        '''
+        callback_list = self._callbacks.setdefault(signal, [])
+        entry = (callback, args)
+        callback_list.append(entry)
+        return lambda : callback_list.remove(entry)
+
+    def trigger_callback(self, signal, *args, **kwargs):
+        'trigger the signal'
+        if self.callback_suppressed(signal):
+            return
+        log.debug('callback triggered: %s %r %r'%(signal, args, kwargs))
+        try:
+            callbacks = self._callbacks.get(signal,())
+            if len(callbacks) == 0:
+                log.warn('%r was triggered, but it has no handlers! '
+                         'maybe it\'s misspelled?'%signal)
+            for callback,curried_args in callbacks:
+                callback(*(curried_args+args), **kwargs)
+        except ShortCircuitCallbacks, e:
+            if len(e.args) > 0:
+                return e.args[0]
+
+    def suppress_callback(self, signal):
+        'any calls to trigger_callback() using signal will be ignored'
+        self._suppressed_callbacks.append(signal)
+
+    def callback_suppressed(self, signal):
+        'query whether signal is being suppressed'
+        return signal in self._suppressed_callbacks
+
+    def resume_callback(self, signal):
+        'opposite of suppress_callback: trigger_callback() will work again'
+        if self.callback_suppressed(signal):
+            self._suppressed_callbacks.remove(signal)
+
+
+def cached_property(producer_func):
+    '''decorator for caching the results of (mostly) static calculations
+
+    If the calculations are not actually static, you can force a
+    recalculation on next attribute access by using del on the attribute
+    to clear the cache. See usage below for more.
+
+    Usage:
+
+    from fitz.misc import cached_property
+    class k:
+        @cached_property
+        def blerg(self):
+            [time-consuming calculation]
+            return value
+    i = k()
+    i.blerg     # does the calculation and returns the value
+    i.blerg     # retrieves value from cache without running the code
+    i.blerg     # still in the cache, so no calculation
+    del i.blerg # clears the cached value
+    i.blerg     # does the calculation again since the cache was cleared
+
+    implementation detail: the attribute "_property_cache" will be added to
+                           the instance
+    '''
+    def cached_property_getter(self):
+        try:
+            cache = self._property_cache
+        except AttributeError:
+            cache = self._property_cache = {}
+        try:
+            return cache[producer_func.__name__]
+        except KeyError:
+            rval = producer_func(self)
+            cache[producer_func.__name__] = rval
+            return rval
+    def cached_property_deleter(self):
+        try:
+            cache = self._property_cache
+        except AttributeError:
+            cache = self._property_cache = {}
+        try:
+            del cache[producer_func.__name__]
+        except KeyError:
+            pass
+    return property(cached_property_getter, None, cached_property_deleter)
+
+
+class AtomicFileWriter(file):
+    '''subclass of file that makes overwriting files an atomic operation
+
+    That is, either the whole file is committed to disk, or else the old
+    version of the file is still in place. This is accomplished by writing
+    the data to x.tmp, and then renaming x.tmp to x when .close() is called.
+    On many filesystems (eg ext3 with the default data=ordered option) this
+    ensures that when the file x already exists, it is "overwritten"
+    atomically.
+
+    This is very useful for program state and config files.
+    '''
+
+    def __init__(self, path, mode='w'):
+        assert 'w' in mode
+        self.final_path = path
+        self.initial_path = path+'.tmp'
+        file.__init__(self, self.initial_path, mode)
+        self._close_has_been_called = False
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        file.close(self)
+        if not self._close_has_been_called:
+            os.rename(self.initial_path, self.final_path)
+            self._close_has_been_called = True
