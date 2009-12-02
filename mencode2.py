d4ecf556e4b9ded1e6bf8e9fb660402b5eb3359f
Richard Darst <rkd@zgib.net>
2009-12-02 04:55:42 +0200

    mencode2.py - Major changes to module to make things more flexible
    darcs-hash:20091202025542-82ea9-df1b74195be228a6a442fa16bdff541b0bd49bd3.gz

--- mencode2.py
+++ mencode2.py
@@ -10,18 +10,40 @@ import sys
 # - run your python script: "python yourscript.py"
 
 var = {}
-var["origfname"] = "rambo2.mpg"
-#var["syncMplayerOpts"] = ["-vf", "harddup", "-ofps", "24000/1001"]
-var["title"] = "Rambo: First Blood Part 2"
+var["input"] = "utah2.mpg"
+var["output"] = var["input"]
+var["mplayeropts"] = ["-vf", "harddup"]
+var["title"] = "Utahraptor Revenge 2"
 #var["fname"] = var["origfname"] + ".norm.avi"
-var["fname"] = var["origfname"]
-var["vf"] = "crop=720:368:0:54"
+#var["vf"] = "crop=720:368:0:54"
 var["aspect"] = "2.35"   # like 4/3, 16/9, 2.35
-var["vbitrate"] = 975
-# var["qp"] = 20
 var["xysize"] = "720x368" # final size
-var["fps"] = "24000/1001"
-var["audioRawRate"] = "48000"
+#var["fps"] = "24000/1001"
+
+
+# You need to pick one of the following modes of encoding.  I
+# recommend either "crf" for constant-quality if you don't care about
+# total size, or if not "vbitrate" for choosing a overall bitrate with
+# two-pass encoding.
+#--------------------------
+# Constant quality mode.  If "crf" is present, it is a one-pass
+# encoding, which only uses x264opts_both and x242opts_pass2.  See
+# `x264 --help`.
+var["crf"] = 23
+# In this mode, also look at these types of options:
+#var["x264opts_both"] = ['--preset', 'veryslow',
+#                        '--tune', 'film',
+#                        ]
+#--------------------------
+# Set the overall bitrate.  This will do two-pass, using "--bitrate
+# NNN" and the x264 two-pass options
+var["vbitrate"] = 975
+#--------------------------
+# This is just like above, but specifies a constast quantizer.  Still
+# does two pass.  Constant quality (above) is better.
+var["qp"] = 30
+#--------------------------
+
 var["x264opts_both"] = ["--threads", "2",
 #                        "--bitrate", str(var["vbitrate"]),
                         "--ref", "4",
@@ -39,6 +61,7 @@ var["x264opts_pass2"] = ["--subme", "6",
                          "--8x8dct",
                          ]
 
+var["audioRawRate"] = "48000"
 audio = [{"aid": 128,
           "oggquality": 3,
           "tracktitle": "English",
@@ -60,19 +83,29 @@ var["audio"] = audio
 doaudio=True
 dovideo=True
 
-#x264 = "x264"
-x264path = "/home/richard/sys/x264/x264/x264"
 
-# audio tracks:
-# 128 - 
-# 137 -
-# 130 - 
-# 131 -
+# Override  these globals
+x264path = "/home/richard/sys/x264/x264"
+mencoderpath = "/home/richard/sys/MPlayer-1.0rc1/mencoder"
 
 
 #
 # Function Definitions
 #
+
+def maketmpnames(var):
+    audio = var["audio"]
+    for i, a in enumerate(audio):
+        a["newindex"] = i
+        a["audiofname"] = "tmp."+var["input"]+".audio."+str(a['aid'])+".ogg"
+        audiopipe = 'tmp.'+var['input']+'.audio.'+str(a['aid'])+'.pipe.wav'
+        a['audiopipe'] = audiopipe
+
+    videopipe = "tmp."+var["input"]+".video.pipe.raw"
+    var["videopipe"] = videopipe
+    videotmp = "tmp."+var["input"]+".video.mkv"
+    var["videotmp"] = videotmp
+
 
 def audioencode(var):
     # This function uses variables from the outside scope to command it.
@@ -82,33 +115,39 @@ def audioencode(var):
               "--quiet",
               "--raw-rate", var["audioRawRate"],
               "-o"+var["audiofname"],
-              "tmp.audio.pipe.wav",
+              var['audiopipe'],
               ]
-    mencoder = ["mencoder",
+    mencoder = [mencoderpath,
                 "-ovc", "copy", "-vc", "dummy",
                 "-aid", str(var["aid"]),
                 "-of", "rawaudio", "-oac", "pcm",
                 "-af", "format=s16le",
-                "-o", "tmp.audio.pipe.wav",
-                var["fname"],
+                "-o", var['audiopipe'],
+                var["input"],
                 ]
-    os.spawnvp(os.P_NOWAIT, "oggenc", oggenc)
-    os.spawnvp(os.P_WAIT, "mencoder", mencoder)
+    if "mencoderopts" in var:
+        mencoder[1:1] = var["mencoderopts"]
+    #os.spawnvp(os.P_NOWAIT, "oggenc", oggenc)
+    #os.spawnvp(os.P_WAIT, "mencoder", mencoder)
+    os.spawnvp(os.P_NOWAIT, mencoderpath, mencoder)
+    os.spawnvp(os.P_WAIT, "oggenc", oggenc)
 
 # 
 # audio part
 # 
 def doaudio(var):
     audio = var["audio"]
-    os.system("mkfifo tmp.audio.pipe.wav")
     for i, a in enumerate(audio):
         print "****Audio track", i
-        a["newindex"] = i
-        a["audiofname"] = "audio"+str(i)+".ogg"
+        #a["newindex"] = i
+        #a["audiofname"] = "tmp."+var["input"]+".audio."+str(a['aid'])+".ogg"
+        #audiopipe = 'tmp.'+var['input']+'.audio.'+str(a['aid'])+'.pipe.wav'
+        #a['audiopipe'] = audiopipe
+        os.system("mkfifo "+a["audiopipe"])
         var2 = var.copy()
         var2.update(a)
         audioencode(var2)
-    os.system("rm tmp.audio.pipe.wav")
+        os.system("rm "+a["audiopipe"])
 
 # 
 # Video Part
@@ -116,46 +155,59 @@ def doaudio(var):
 def dovideo(var):
     # This copy below isn't needed if the audio copies from above are left over
     print "****Beginning video stuff"
-    os.system("mkfifo tmp.video.pipe.raw")
+    #videopipe = "tmp."+var["input"]+".video.pipe.raw"
+    #videotmp = "tmp."+var["input"]+".video.mkv"
+    #var["videotmp"] = videotmp
+    videopipe = var["videopipe"]
+    videotmp = var["videotmp"]
+    os.system("mkfifo "+videopipe)
     
-    mencoder = ["mencoder",
+    mencoder = [mencoderpath,
+                #"-quiet",
                 "-oac", "copy", "-ovc", "raw", "-of", "rawvideo",
-                "-o", "tmp.video.pipe.raw",
                 "-ofps", var["fps"],
+                "-o", videopipe,
                 #-vf crop=720:464:0:6,scale=704:384,dsize=-1,format=I420
-                var["fname"]]
-    if var.has_key("vf"):
+                var["input"]]
+    if "vf" in var:
         mencoder[1:1] = ["-vf", var["vf"]+",dsize=-1,format=I420"]
     else:
         mencoder[1:1] = ["-vf", "dsize=-1,format=I420"]
+    if "mencoderopts" in var:
+        mencoder[1:1] = var["mencoderopts"]
         
     #if var.has_key("mplayeropts"):
     #    mencoder[1:1] = var["mplayeropts"]
     x264 = [x264path,
-            "--progress",
-            "--stats", "x264_2pass.log",
-            "tmp.video.pipe.raw", var["xysize"], "--fps", var["fps"]]
+            #"--progress",
+            "--stats", "tmp."+var["input"]+".x264_2pass.log",
+            videopipe, var["xysize"], "--fps", var["fps"],
+            "--sar", var["aspect"],
+            "--verbose"]
 
-    x264[1:1] = var["x264opts_both"]
+    if "x264opts_both" in var:
+        x264[1:1] = var["x264opts_both"]
 
-    if var.has_key("vbitrate"):
+    if "vbitrate" in var:
         x264[1:1] = ["--bitrate", str(var["vbitrate"])]
-    elif var.has_key("qp"):
+    elif "qp" in var:
         x264[1:1] = ["--qp", str(var["qp"])]
-    elif var.has_key("crf"):
+    elif "crf" in var:
         x264[1:1] = ["--crf", str(var["crf"])]
     else:
         print "either key of 'vbitrate', 'qp', or 'crf' must be specified."
         sys.exit()
 
+    # "crf" is a one-pass encoding.
     if var.has_key("crf"):
         x264_pass2 = x264[:]
         #x264_pass2[1:1] = ["--pass", "2"]
-        x264_pass2[1:1] = ["-o", "video.x264.mkv"]
-        x264_pass2[1:1] = var["x264opts_pass2"]
+        x264_pass2[1:1] = ["-o", videotmp]
+        if "x264opts_pass2" in var:
+            x264_pass2[1:1] = var["x264opts_pass2"]
 
         print "****Beginning pass 2 (with crf)"
-        os.spawnvp(os.P_NOWAIT, "mencoder", mencoder)
+        os.spawnvp(os.P_NOWAIT, mencoderpath, mencoder)
         os.spawnvp(os.P_WAIT, x264path, x264_pass2)
 
     else:
@@ -164,22 +216,24 @@ def dovideo(var):
     
         x264_pass1[1:1] = ["--pass", "1"]
         x264_pass1[1:1] = ["-o", "/dev/null"]
-        x264_pass2[1:1] = var["x264opts_pass1"]
+        if "x264opts_pass1" in var:
+            x264_pass2[1:1] = var["x264opts_pass1"]
     
         x264_pass2[1:1] = ["--pass", "2"]
-        x264_pass2[1:1] = ["-o", "video.x264.mkv"]
-        x264_pass2[1:1] = var["x264opts_pass2"]
+        x264_pass2[1:1] = ["-o", videotmp]
+        if "x264opts_pass2" in var:
+            x264_pass2[1:1] = var["x264opts_pass2"]
     
         print "****Beginning pass 1"
-        os.spawnvp(os.P_NOWAIT, "mencoder", mencoder)
+        os.spawnvp(os.P_NOWAIT, mencoderpath, mencoder)
         os.spawnvp(os.P_WAIT, x264path, x264_pass1)
     
         print "****Beginning pass 2"
-        os.spawnvp(os.P_NOWAIT, "mencoder", mencoder)
+        os.spawnvp(os.P_NOWAIT, mencoderpath, mencoder)
         os.spawnvp(os.P_WAIT, x264path, x264_pass2)
 
     print "****Done with video encoding"
-    os.system("rm tmp.video.pipe.raw")
+    os.system("rm "+videopipe)
     
     
 def merge(var):
@@ -188,33 +242,84 @@ def merge(var):
     mkvmerge = ["mkvmerge",
                 "--title",  var["title"],
                 "--noaudio", ]
-    if var.has_key("aspect"):
+    if "aspect" in var:
         mkvmerge.extend(["--aspect-ratio", "1:"+var["aspect"]])
-    mkvmerge.append("video.x264.mkv")
-    
+    mkvmerge.append(var["videotmp"])
+
     for i, a in enumerate(audio):
-        a["audiofname"] = "audio"+str(i)+".ogg"
+        #a["audiofname"] = a["audiofname"]
         y = ["--novideo", a["audiofname"] ]
         if a.has_key("language"):
             y[0:0] = ["--language", "0:"+a["language"]]
         mkvmerge.extend(y)
     # what output filename to use:
-    if var.has_key("vbitrate"):
+    if "vbitrate" in var:
         brname = str(var["vbitrate"])
-    elif var.has_key("qp"):
+    elif "qp" in var:
         brname = "q"+str(var["qp"])
-    elif var.has_key("crf"):
+    elif "crf" in var:
         brname = "c"+str(var["crf"])
     else:
         brname = "x"
 
-    mkvmerge.extend(["-o", var["origfname"]+".x264."+brname+".mkv"])
+    mkvmerge.extend(["-o", var["output"]+".x264."+brname+".mkv"])
     os.spawnvp(os.P_WAIT, "mkvmerge", mkvmerge)
 
 def doall(var):
+    maketmpnames(var)
     doaudio(var)
     dovideo(var)
     merge(var)
     
 if __name__ == "__main__":
     doall(var)
+
+
+# Here is one example of a sample script.  Basically use this
+# interface to design a script to do what you need to do:
+'''
+var = { }
+var["input"] = "d1-1.dump"
+var["output"] = "btvs401"
+var["title"] = "BTvS - 4.01"
+var["aspect"] = "4/3"
+var["fps"] = "24000/1001"
+#var["xysize"] = "720x480"
+var["xysize"] = "540x360"
+var["vf"] = "yadif,scale=540:360"
+#var["mencoderopts"] = ["-endpos", "4:00"]
+var["audioRawRate"] = "48000"
+
+var["crf"] = 23
+var["x264opts_both"] = ['--preset', 'veryslow',
+                        '--tune', 'film',
+                        ]
+
+audio = [{"aid": 128,
+          "oggquality": 3,
+          "tracktitle": "English",
+          "language": "en",
+          },
+          #{"aid": 129,
+          # "oggquality": 1,
+          # "tracktitle": "French",
+          # "language": "fr",
+          # },
+          #{"aid": 130,
+          # "oggquality": 1,
+          # "tracktitle": "Spanish",
+          # "language": "es",
+          # },
+          ]
+var['audio'] = audio
+
+
+# Individual pieces (for testing):
+# mencode2.maketmpnames(var)
+# mencode2.doaudio(var)
+# mencode2.dovideo(var)
+# mencode2.merge(var)
+
+# All at once (calls the function above in sequence):
+mencode2.doall(var)
+'''
