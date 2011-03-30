2d5c90999245006af470e19c2e1ea03582bacb73
Richard Darst <rkd@zgib.net>
2011-03-30 20:43:46 +0300

    Add ircstats.py and ircstats2.py - examples for irclogparser.py
    Ignore-this: 6aa66c5cf56a1eb8874a87d88018e1d1
    
    darcs-hash:20110330174346-82ea9-c043b95cfc638d96166a738626b31f586df25a57.gz

---
+++ ircstats.py
@@ -0,0 +1,87 @@
+import re
+import sys
+
+if len(sys.argv) > 1:
+    f = open(sys.argv[1])
+else:
+    f = open('irclogs/oftc/#debconf-team.log')
+from fitz.mathutil import Averager
+class NickStats(object):
+    def __init__(self):
+        self.lines = 0
+        self.allLines = 0
+        self.chars = 0
+        self.lastline = 0
+        self.maxInARow = 0
+        self.maxCharsInARow = 0
+        self._lastInARow = 0
+        self._lastCharsInARow = 0
+        self.LineStats = Averager()
+        self._lengthsList = [ ]
+    def __iadd__(self, (lineno, line)):
+        # how many lines in a row, on average?
+        if lineno == self.lastline+1:
+            self._lastInARow += 1
+            self._lastCharsInARow += len(line)
+        else:
+            self.maxInARow = max(self.maxInARow, self._lastInARow)
+            self.maxCharsInARow = max(self.maxCharsInARow, self._lastCharsInARow)
+            self._lastInARow = 1
+            self._lastCharsInARow = 1
+        # count of all lines, even shourt ones:
+        self.allLines += 1
+        # filter which lines to consider
+        if len(line) < 10: return self
+        # basic stats
+        self.LineStats.add(len(line))
+        self.lines += 1
+        self.chars += len(line)
+        self._lengthsList.append(len(line))
+        # record the last line we saw and continue
+        self.lastline = lineno
+        return self
+    @property
+    def longFrac(self):
+        return float(self.lines) / self.allLines
+    @property
+    def mean(self):
+        return self.LineStats.mean
+    @property
+    def std(self):
+        return self.LineStats.std
+    @property
+    def median(self):
+        if len(self._lengthsList) == 0: return float('nan')
+        index = len(self._lengthsList)//2
+        return sorted(self._lengthsList)[index]
+    @property
+    def skew(self):
+        if self.std == 0: return float('nan')
+        return 30*(self.mean - self.median)/self.std
+
+re_msg = re.compile('(?P<time>[^ ]+) <.?(?P<nick>[^ :#]*)(?:#[^ ]+)?> (?P<line>.*)')
+nicks = { }
+lineno = 0
+for rawlineno, line in enumerate(f):
+    m = re_msg.match(line)
+    #print line, m
+    if not m: continue
+    lineno += 1
+    l = m.groupdict()
+    if l['nick'] not in nicks:
+        nicks[l['nick']] = NickStats()
+    #print nicks[l['nick']]
+    nicks[l['nick']] += (lineno, l['line'])
+    #nicks[l['nick']][1] += len(line)
+
+    #if lineno > 500: break
+nicks_sorted = reversed(sorted((nicks.iteritems()), key=lambda x:x[1].lines ))
+print "totals:", sum( [v.lines for v in nicks.itervalues()] ), sum( [v.chars for v in nicks.itervalues()] )
+print "%-15s %6s %7s %5s %5s %5s %4s %4s %3s %5s"%(
+    'nick', 'lines', 'chars', '>10', 'mean', 'media', 'std', 'skew', 'rw', 'chrw')
+for n,v in nicks_sorted:
+   try:
+    print "%-15s %6d %7d %5.2f %5.1f %5.1f %4.1f %4.1f %3d %5d"%(
+        n, v.lines, v.chars, v.longFrac, v.mean, v.median, v.std, v.skew, v.maxInARow, v.maxCharsInARow)
+   except IOError:
+       sys.exit()
