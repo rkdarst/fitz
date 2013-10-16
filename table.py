ee536710851f8562c896171cfe212b57790335d7
Richard Darst <rkd@zgib.net>
2013-10-16 18:17:29 +0300

    table.py: writing and transforming functions
    Ignore-this: b2bc49ccf04f9881d71d2d75fddab16e
    
    darcs-hash:20131016151729-82ea9-4f4b13f0db5b36005a779670e0e669eb45dee561.gz

--- table.py
+++ table.py
@@ -1,6 +1,7 @@
 # Richard Darst, 2013 April
 
 import collections
+import re
 import sys
 
 class TablePrinter(object):
@@ -12,3 +13,61 @@ class TablePrinter(object):
             self.widths[i] = max(self.widths[i], len(arg))
             print >> fout, arg.ljust(self.widths[i]),
         print >> fout
+
+
+def write_table(fobj, table, headers=None, labels=None):
+    for line in headers:
+        print >> fobj, "#", headerline
+    print >> fobj, "#", time.ctime()
+    print >> fobj, "#"+ " ".join("%d:%s"%(i+1,x) for i,x in enumerate(labels))
+
+    for line in table:
+        for x in line:
+            print >> fobj, x,
+        print >> fobj
+
+def read_table(f, header=False):
+    lines = [ ]
+    headers = [ ]
+    labels = None
+    for line in f:
+        if line.strip().startswith('#'):
+            m = re.findall('(\d+):(\S+)', line)
+            if m:  # m is a list of (group1, group2)
+                indices, labels = zip(*m)
+            continue
+        if line.strip().startswith('#'):
+            headers.append(line[1:].strip())
+            continue
+        line = line.split()
+        line = [ float(x) for x in line ]
+        lines.append(line)
+    if header:
+        return lines, headers, labels
+    return lines
+
+
+def transform_tables(transformations, *tables):
+    """Input two tables, output transformation of them
+
+    aN is first table, bN is second table, etc.
+    a0 = 1st element in table 0, a1 = 2nd element in table 0, etc.
+
+    Transformations is a comma-separated list of expressions to be
+    python eval()'ed with the a0, a1, b0, b1, etc, variables defined.
+    """
+    transformations = transformations.split(',')
+    newtable = [ ]
+    import math
+    math_vars = dict((k, v) for k,v in math.__dict__.iteritems()
+                     if not k.startswith('_'))
+    for rows in zip(*tables):
+        newtable.append([])
+        # Generate variables
+        vars = math_vars.copy()
+        for rowid, row in enumerate(rows):
+            for colid, col in enumerate(row):
+                vars["%s%d"%(string.lowercase[rowid], colid)] = col
+        for t in transformations:
+            newtable[-1].append(eval(t, vars))
+    return newtable
