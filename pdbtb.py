eb08e0d002019d0fa6bfbd6e44079a774a8b0636
Richard Darst <rkd@zgib.net>
2013-11-21 18:44:45 +0200

    Support running from ipython
    Ignore-this: 41bff76afcb57a5b7d4c0b132ead2386
    
    ipython overrides sys.excepthook on every call, so we can't use it.
    Make a new function that traps the exception, and runs pdb.post_mortem
    using that.
    
    darcs-hash:20131121164445-82ea9-d29ee415db72fe118797b1874989946414b4f95e.gz

--- pdbtb.py
+++ pdbtb.py
@@ -14,6 +14,22 @@ def enable():
 def disable():
     sys.excepthook = sys.__excepthook__
 
+def run(func, *args, **kwargs):
+    enable()
+    func(*args, **kwargs)
+
+def run_ipython(func, *args, **kwargs):
+    try:
+        func(*args, **kwargs)
+    except Exception as e:
+        t, value, tb = sys.exc_info()
+        sys.__excepthook__(t, value, tb)
+        frame = sys.exc_info()[2]
+        #tb = e.tb_frame
+        pdb.post_mortem(tb)
+        del frame
+
+
 if __name__ == "__main__":
     import fitz.runpy2
     enable()
