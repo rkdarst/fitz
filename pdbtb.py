6f0822975b73ac45f126deac8afff781a1677e25
Richard Darst <rkd@zgib.net>
2013-11-21 19:49:29 +0200

    pdbtb.py: has a .now() function for immediate pdb
    Ignore-this: 7b4f5405c97cfe43a3c0fa8185daeb0a
    
    darcs-hash:20131121174929-82ea9-b2e2c6ac5f61247381c70b0b5a7fb52acada077f.gz

--- pdbtb.py
+++ pdbtb.py
@@ -3,31 +3,53 @@
 """
 """
 
+import inspect
 import pdb
 import sys
 
 def excepthookpdb(t, value, tb):
+    """Exception hook to invoke pdb on exceptions."""
     sys.__excepthook__(t, value, tb)
     pdb.post_mortem(tb)
 def enable():
+    """Enable exception hook."""
     sys.excepthook = excepthookpdb
 def disable():
+    """Disable exception hook."""
     sys.excepthook = sys.__excepthook__
 
 def run(func, *args, **kwargs):
+    """Run a function with pdb exception hook enabled."""
     enable()
     func(*args, **kwargs)
 
 def run_ipython(func, *args, **kwargs):
+    """ipython pdb hook: invokes pdb on exceptions in ipython.
+
+    The function func is called, with arguments args and
+    kwargs=kwargs.  If this func raises an exception, pdb is invoked
+    on that frame.  Upon exit from pdb, return to ipython normally."""
     try:
         func(*args, **kwargs)
     except Exception as e:
-        t, value, tb = sys.exc_info()
+        #t, value, tb = sys.exc_info()
         sys.__excepthook__(t, value, tb)
         frame = sys.exc_info()[2]
         #tb = e.tb_frame
         pdb.post_mortem(tb)
-        del frame
+        del frame   # the docs warn to avoid circular references.
+
+def now(frame=None, stackLevel=1):
+    """Run pdb in the calling frame."""
+    if frame is None:
+        frame = inspect.stack()[stackLevel][0]
+    p = pdb.Pdb()
+    def do_quit(self, arg):
+        sys.exit()
+    p.do_quit = type(p.do_quit)(do_quit, p, pdb.Pdb)
+    p.do_q = p.do_quit
+    p.reset()
+    p.interaction(frame, None)
 
 
 if __name__ == "__main__":
