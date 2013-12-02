8517b214d4fb89aafe9d8753ea21a154f0de9bf4
Richard Darst <rkd@zgib.net>
2013-12-02 12:08:43 +0200

    pdbtb: history saving and restoration
    Ignore-this: 900b7c474e9dd8d1a02148fa628160eb
    
    darcs-hash:20131202100843-82ea9-d5aa43ee1f245695b3575d90e0966116464b81c2.gz

--- pdbtb.py
+++ pdbtb.py
@@ -23,21 +23,45 @@ def _run(func, *args, **kwargs):
     enable()
     func(*args, **kwargs)
 
+_run_history = [ ]
+import readline
+def _get_history(first=1):
+    return [ readline.get_history_item(x)
+             for x in range(first, readline.get_current_history_length()+1) ]
+def _add_history(hist):
+    [ readline.add_history(x) for x in hist ]
+def _restore_history(hist):
+    readline.clear_history()
+    _add_history(hist)
+
 def run(func, *args, **kwargs):
     """pdb hook: invokes pdb on exceptions in python.
 
     The function func is called, with arguments args and
     kwargs=kwargs.  If this func raises an exception, pdb is invoked
     on that frame.  Upon exit from pdb, return to python normally."""
+    # save history
+    old_hist = _get_history()
+    old_hist_start = readline.get_current_history_length()+1
+
     try:
         return func(*args, **kwargs)
     except Exception as e:
+        _add_history(_run_history)
+
+
         t, value, tb = sys.exc_info()
         sys.__excepthook__(t, value, tb)
         frame = sys.exc_info()[2]
         #tb = e.tb_frame
         pdb.post_mortem(tb)
         del frame   # the docs warn to avoid circular references.
+        del t, value, tb
+
+        _run_history[:] = _get_history(first=old_hist_start)
+    readline.clear_history()
+    _restore_history(old_hist)
+    print old_hist
 run_ipython = run
 
 def now(frame=None, stackLevel=1):
